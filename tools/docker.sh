1e076d5eada9ca52a7d539447a28bb81cde1e059
woongjang <woongjang@idatabank.com>
2021-07-30 09:21:37 +0900

    프로메테우스 설치 , Grafana 설치후 테스트

--- tools/docker.sh
+++ tools/docker.sh
@@ -286,12 +286,9 @@ kubectl delete -f success2.yaml # delete
 
 #3장 도커 빌드후에 도커 레지스트리로 올리기
 
-~/_Book_k8sInfra/ch5/5.3.1/nfs-exporter.sh jenkins
-ls -n /nfs_shared
-chown 1000:1000 /nfs_shared/jenkins/  # jenkins nfs owner ,group id 
-ls -n /nfs_shared
 
-kubectl exec -it jenkins~ -- cat /etc/passwd
+
+
 # https://github.com/ramitsurana/awesome-kubernetes
 ## https://betterprogramming.pub/awesome-kubernetes-command-line-hacks-8bd3604e394f
 alias k=kubectl
@@ -303,3 +300,224 @@ alias kdb='kubectl describe'
 alias kl='kubectl logs'
 alias ke='kubectl exec -it'
 alias kgp='kubectl get pod -o wide'
+
+##  Jenkins 설치
+
+~/_Book_k8sInfra/ch5/5.3.1/nfs-exporter.sh jenkins
+ls -n /nfs_shared
+chown 1000:1000 /nfs_shared/jenkins/  # jenkins nfs owner ,group id 
+ls -n /nfs_shared
+kubectl apply -f ~/_Book_k8sInfra/ch5/5.3.1/jenkins-volume.yaml
+#pv 확인 10Gi
+kubectl get pv jenkins
+#pvc 확인
+kubectl get pvc jenkins
+ ~/_Book_k8sInfra/ch5/5.3.1/jenkins-install.sh
+ '''
+ admin/admin
+ 1. Get your 'admin' user password by running:
+  printf $(kubectl get secret --namespace default jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode);echo
+2. Get the Jenkins URL to visit by running these commands in the same shell:
+  NOTE: It may take a few minutes for the LoadBalancer IP to be available.
+        You can watch the status of by running 'kubectl get svc --namespace default -w jenkins'
+  export SERVICE_IP=$(kubectl get svc --namespace default jenkins --template "{{ range (index .status.loadBalancer.ingress 0) }}{{ . }}{{ end }}")
+  echo http://$SERVICE_IP:80/login
+
+  jenkins   LoadBalancer   10.105.187.154   192.168.1.11   80:32332/TCP   5m5s
+  http://192.168.1.11:80/login
+
+
+3. Login with the password from step 1 and the username: admin
+
+4. Use Jenkins Configuration as Code by specifying configScripts in your values.yaml file, see documentation: http:///configuration-as-code and examples: https://github.com/jenkinsci/configuration-as-code-plugin/tree/master/demos
+
+For more information on running Jenkins on Kubernetes, visit:
+https://cloud.google.com/solutions/jenkins-on-container-engine
+For more information about Jenkins Configuration as Code, visit:
+
+ '''
+kubectl get deployment
+kubectl get pod
+kubectl exec -it jenkins-6b6f9b8675-t5trr -- cat /etc/passwd
+
+### jenkins 삭제 초기ㅗ하
+kubectl get deployment
+kubectl get pods
+helm uninstall jenkins
+rm -rf /nfs_shared/jenkins/*
+ ~/_Book_k8sInfra/ch5/5.3.1/jenkins-install.sh
+
+ ##
+ kubectl get services
+ ### 
+ kubectl get serviceaccount
+ ## 권한 부여 
+ kubectl create clusterrolebinding jenkins-cluster-admin \
+  --clusterrole=cluster-admin --serviceaccount=default:jenkins
+  ## 권한 부여 결과 output
+  clusterrolebinding.rbac.authorization.k8s.io/jenkins-cluster-admin created
+  ## yaml로 output 권한 보기
+  kubectl get clusterrolebinding jenkins-cluster-admin -o yaml
+
+  freestyle로 jenkins설정
+  https//github.com/iac-source/echo-ip
+https://github.com/changwng/echo-ip 을 CI/CD로 한다.
+https://github.com/changwng/blue-green  : 블루그린 방식 사용 CI/CD
+#jenkins build Exucte shell입력 스크립트
+docker build -t 192.168.1.10:8443/echo-ip .
+docker push 192.168.1.10:8443/echo-ip
+kubectl create deployment fs-echo-ip --image=192.168.1.10:8443/echo-ip
+kubectl expose deployment fs-echo-ip --type=LoadBalancer --name=fs-echo-ip-svc --port=8080 --target-port=80
+#jenkins에서 빌드 하면 http://192.168.1.12:8080/로 접근하면된다.
+kubectl delete service fs-echo-ip-svc
+kubectl delete deployment fs-echo-ip
+
+
+### 블루 그린 배포 전략으로 배포 처리 한후에
+kubectl get deployments,service --selector=app=dashboard
+## 전체 배포된것 삭제 처리
+kubectl delete deployments,service,configmap --selector=app=dashboard
+kubectl config view
+
+# github 기준으로 지속적 배포 작업 진행 하도록 하는것
+https://github.com/Woong-For-Mohani/GitOpts.git mymohani@gmail.com /cw89qn~ , rrn!!
+https://192.168.1.10:6443
+
+## k-k8s 머신에서 홈디렉토리
+mkdir ~/gitopts
+cd gitopts
+git init
+
+## github 자격증명 helper설정
+git config --global user.name "mymohani"
+git config --global user.email "mymohani@gmail.com"
+git config --global credential.helper "store --file ~/.git-cred"
+
+git remote add origin https://github.com/Woong-For-Mohani/GitOpts.git
+cp ~/_Book_k8sInfra/ch5/5.5.1/* ~/gitopts  #사전 준비 파일 복사
+sed -i 's,Git-URL,https://github.com/Woong-For-Mohani/GitOpts.git,g' Jenkinsfile
+git add .
+git config --list
+git commit -m "init commit"
+git branch -M main # -M(move)
+git push -u origin main # -u (--set-upstream)
+
+#jenkins에서 지속적 통합으로 세팅 : kuberanate continuous Deploy 플러그인 설치
+#Jenkins Home> 관리> Manage Credentials로 가서 쿠버네티스용 지속적 배포 플러그인의 새로운 자격증명 정보 추가
+# 쿠버 설정 파일에 대한 자격증명 가져오려면 현재 쿠버 설정 마스터 노드 m-k8s에 접속 권한이 필요함
+# 쿠버에서 m-k8s 마스터 노드에 접속 가능한 SSH및 kubeconfig 접속을 위한 인증 정보 설정
+# New Item에 dpy-pl-gitops 로 하여 Pipeline으로 설정
+# Poll SCM 에서 schedule에 */10 * * * * 로 크론 표현식을 입력 10분마다 실행
+# Jenkins 표현식은 H/* * * * * 로 1분마다 한번씩
+# gitops-nginx로 배포되었는지 확인
+kubectl get deployments
+sed -i 's/replicas: 2/replicas: 5/' deployment.yaml
+git add . ; git commit -m "change replicas count"; git push -u origin main
+
+# slack noti 연결
+팀 하위 도메인: mthinkws
+통합 토큰 자격 증명 ID: swbdsnolMfWIYo4M88B0y8Pb 토큰을 값으로 사용하여 암호 텍스트 자격 증명을 생성합니다.
+
+cp ~/_Book_k8sInfra/ch5/5.5.2/Jenkinsfile ~/gitop
+
+m-k8s 에서
+bpytop : 리소스 모니 터링
+#프로메테우스로 모니터링 데이터 수집과 통합
+- prometheus-server : 수집기, 시계열 데이터베이스, 웹 ㅕㅑ
+- node-exporter : 외부로 공개 역할
+- kube-state-metrics: 프로메테우스 서비가 수집할수 있는 메트릭 데이터 변환후 공개
+- alertmanager : alert 규칙 설정, 메세지 대상에 전달
+- 푸시 게이트 웨이(push gateway) : 배치와 스케줄 작업시 수행되는 일회성 작업들의 상태 저장 프로메테우스가 주기적으로 가져갈수 있도록
+  공개
+
+  # 프로메테우스 설치
+ # 1. 공유 디렉토리 생성후 pv, pvc 생성및 할당 접근 ID : 1000
+    ~/_Book_k8sInfra/ch6/6.2.1/prometheus-server-preconfig.sh
+    #---------------------------------------------
+    /nfs_shared/prometheus/server
+    [Step 4/4] Task [Create PV,PVC for prometheus-server]
+    persistentvolume/prometheus-server created
+    persistentvolumeclaim/prometheus-server created
+   #---------------------------------------------  
+#2. 프로메테우스 설치
+ ~/_Book_k8sInfra/ch6/6.2.1/prometheus-install.sh
+ 설치 확인
+ kubectl get pods --selector=app=prometheus
+ kubectl get pods --selector=app=prometheus -o wide
+
+ ##--------------- out put 
+        The Prometheus server can be accessed via port 80 on the following DNS name from within your cluster:
+        prometheus-server.default.svc.cluster.local
+
+
+        Get the Prometheus server URL by running these commands in the same shell:
+        NOTE: It may take a few minutes for the LoadBalancer IP to be available.
+                You can watch the status of by running 'kubectl get svc --namespace default -w prometheus-server'
+
+        export SERVICE_IP=$(kubectl get svc --namespace default prometheus-server -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+        echo http://$SERVICE_IP:80
+
+             http://192.168.1.14:80  : 현재 설치된 서버에서 확인
+
+        #################################################################################
+        ######   WARNING: Pod Security Policy has been moved to a global property.  #####
+        ######            use .Values.podSecurityPolicy.enabled with pod-based      #####
+        ######            annotations                                               #####
+        ######            (e.g. .Values.nodeExporter.podSecurityPolicy.annotations) #####
+        #################################################################################
+        ip to coredns 질의문
+        kubectl run net --image=sysnet4admin/net-tools --restart=Never --rm -it -- nslookup 192.168.1.14
+ ##--------------- out put 
+
+kubectl get configmap prometheus-server -o yaml | nl
+## 프로메테우스 exporter 설정
+kubectl apply -f ~/_Book_k8sInfra/ch6/6.2.3/nginx-status-annot.yaml
+cat ~/_Book_k8sInfra/ch6/6.2.3/nginx-status-annot.yaml | nl
+# 이미 만든 메트릭스 공개
+kubectl apply -f ~/_Book_k8sInfra/ch6/6.2.3/nginx-status-metrics.yaml
+# 메트릭스 삭제
+kubectl delete -f ~/_Book_k8sInfra/ch6/6.2.3/nginx-status-metrics.yaml
+
+# 헬름으로 그라파나 설치 하기
+~/_Book_k8sInfra/ch6/6.4.1/grafana-preconfig.sh
+~/_Book_k8sInfra/ch6/6.4.1/grafana-install.sh
+# 공유 폴더 생성
+/nfs_shared/grafana
+### --------------------------------
+1. Get your 'admin' user password by running:
+
+   kubectl get secret --namespace default grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+
+2. The Grafana server can be accessed via port 80 on the following DNS name from within your cluster:
+
+   grafana.default.svc.cluster.local
+
+   Get the Grafana URL to visit by running these commands in the same shell:
+NOTE: It may take a few minutes for the LoadBalancer IP to be available.
+        You can watch the status of by running 'kubectl get svc --namespace default -w grafana'
+     export SERVICE_IP=$(kubectl get svc --namespace default grafana -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+     http://$SERVICE_IP:80
+### ------------------------------------
+kubectl get deployment grafana
+#--- 노드 네트워크 평균 송신/수신 트래픽
+avg(rate(node_network_transmit_bytes_total[5m])) by (kubernetes_node)
+{{ kubernetes_node}} - transmit
+Y축: Data(Metric) -> bytes(Metric)
+-- Query 추가
+avg(rate(node_network_receive_bytes_total[5m])) by (kubernetes_node) * -1
+{{ kubernetes_node}} - receive
+
+노드 상태
+up{job="kubernetes-nodes"}
+{{instance}}
+
+label_vaules(kube_pod_info,namespace)
+
+# cubectl 자동완성 사용하기
+~/_Book_k8sInfra/app/A.kubectl-more/bash-completion.sh
+~/_Book_k8sInfra/app/A.kubectl-more/k8s_rc.sh
+
+# docker container 런타임 저장
+docker run -d nginx
+docker export 컨테이너 > nginx.tar
+tar -C nginx-container -xvf nginx.tar
